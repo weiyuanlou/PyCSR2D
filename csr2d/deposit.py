Fixed a bug in potential calculation ( 1/2 rho factor in zm ). More analysis/plots in csr_test.ipynb ( to be cleaned ).

csr2d/deposit.py
@@ -1,4 +1,6 @@
 import numpy as np
+from numba import jit
+import math
 
 def split_particles(position, charge, mins, maxs, sizes):
     '''
@@ -98,7 +100,76 @@ def deposit_particles(Np, sizes, indexes, contrib):
         charge_grid[ip-1][jp-1] = charge_grid[ip-1][jp-1] + t1
         charge_grid[ip-1][jp]   = charge_grid[ip-1][jp]   + t2
 
-        charge_grid[ip][jp-1]   = charge_grid[ip][jp-1]   + t3
-        charge_grid[ip][jp]     = charge_grid[ip][jp]     + t4
+        charge_grid[ip][jp]     = charge_grid[ip][jp]     + t3
+        charge_grid[ip][jp-1]   = charge_grid[ip][jp-1]   + t4
         
     return charge_grid
+
+
+@jit
+def histogram_cic_2d( q1, q2, w,
+    nbins_1, bins_start_1, bins_end_1,
+    nbins_2, bins_start_2, bins_end_2 ):
+    """
+    Return an 2D histogram of the values in `q1` and `q2` weighted by `w`,
+    consisting of `nbins_1` bins in the first dimension and `nbins_2` bins
+    in the second dimension.
+    Contribution to each bins is determined by the
+    Cloud-in-Cell weighting scheme.
+
+    Source: 
+    ----------
+        https://github.com/openPMD/openPMD-viewer/blob/dev/openpmd_viewer/openpmd_timeseries/utilities.py
+    ----------
+    
+    Parameters:
+    ----------
+    q1/q2 : float, array
+            q1/q2 position of the particles
+    w: float, array
+            weights (charges) of the particles
+    nbins_1/nbins_2 : int
+            number of bins (vertices) in the q1/q2 direction
+    bins_start_1, bins_end_1, bins_start_2, bins_end_2: float
+            start/end value in the q1/q2 direction
+    ----------
+    
+    Returns:
+    ----------
+    A 2D array of size (nbins_1, nbins_2)
+    ----------
+    """
+    # Define various scalars
+    bin_spacing_1 = (bins_end_1-bins_start_1)/(nbins_1-1)
+    inv_spacing_1 = 1./bin_spacing_1
+    bin_spacing_2 = (bins_end_2-bins_start_2)/(nbins_2-1)
+    inv_spacing_2 = 1./bin_spacing_2
+    n_ptcl = len(w)
+
+    # Allocate array for histogrammed data
+    hist_data = np.zeros( (nbins_1, nbins_2), dtype=np.float64 )
+
+    # Go through particle array and bin the data
+    for i in range(n_ptcl):
+
+        # Calculate the index of lower bin to which this particle contributes
+        q1_cell = (q1[i] - bins_start_1) * inv_spacing_1
+        q2_cell = (q2[i] - bins_start_2) * inv_spacing_2
+        i1_low_bin = int( math.floor( q1_cell ) )
+        i2_low_bin = int( math.floor( q2_cell ) )
+
+        # Calculate corresponding CIC shape and deposit the weight
+        S1_low = 1. - (q1_cell - i1_low_bin)
+        S2_low = 1. - (q2_cell - i2_low_bin)
+        if (i1_low_bin >= 0) and (i1_low_bin < nbins_1):
+            if (i2_low_bin >= 0) and (i2_low_bin < nbins_2):
+                hist_data[ i1_low_bin, i2_low_bin ] += w[i]*S1_low*S2_low
+            if (i2_low_bin+1 >= 0) and (i2_low_bin+1 < nbins_2):
+                hist_data[ i1_low_bin, i2_low_bin+1 ] += w[i]*S1_low*(1.-S2_low)
+        if (i1_low_bin+1 >= 0) and (i1_low_bin+1 < nbins_1):
+            if (i2_low_bin >= 0) and (i2_low_bin < nbins_2):
+                hist_data[ i1_low_bin+1, i2_low_bin ] += w[i]*(1.-S1_low)*S2_low
+            if (i2_low_bin+1 >= 0) and (i2_low_bin+1 < nbins_2):
+                hist_data[ i1_low_bin+1, i2_low_bin+1 ] += w[i]*(1.-S1_low)*(1.-S2_low)
+
+    return( hist_data )

csr2d/simple_track.py
@@ -18,7 +18,7 @@ def track_a_bend(b, P0c, L=0, theta = 0, g_err=0):
     
     Input:
         b: initial 6D bmad beam coord
-        P0c: reference momentum in MeV/c
+        P0c: reference momentum in eV/c
         L: length to track in m
         theta: bending angle 
         g_err: error in g = theta/L
@@ -60,9 +60,9 @@ def track_a_bend(b, P0c, L=0, theta = 0, g_err=0):
     Lc = np.sqrt(Lcu**2 + Lcv**2)
     Lp = Lc / sinc(theta_p / 2)
     
-    P = P0c*(1 + pz) # in MeV/c
-    E = np.sqrt(P**2 + 0.510998950**2) # in MeV 
-    E0 = np.sqrt(P0c**2 + 0.510998950**2) # in MeV 
+    P = P0c*(1 + pz) # in eV
+    E = np.sqrt(P**2 + 510998.950**2) # in eV 
+    E0 = np.sqrt(P0c**2 + 510998.950**2) # in eV 
     beta = P/E
     beta0 = P0c/E0
     
@@ -76,7 +76,7 @@ def track_a_bend(b, P0c, L=0, theta = 0, g_err=0):
     return np.array([xf, pxf, yf, pyf, zf, pzf])
     
     
-def track_entrance(d, P0c, L=0, theta = 0, g_err=0, e1=0, f_int = 0, h_gap = 0):
+def track_entrance(d, L=0, theta = 0, g_err=0, e1=0, f_int = 0, h_gap = 0):
     """
     Tracks a 6-D beam through the entrance fringe of a bending magnet.
     See chapter 16.2 of the Bmad manual.
@@ -84,7 +84,6 @@ def track_entrance(d, P0c, L=0, theta = 0, g_err=0, e1=0, f_int = 0, h_gap = 0):
     
     Input:
         b: initial 6D bmad beam coord
-        P0c: reference momentum
         L: length to track in m
         theta: bending angle in rad
         g_err: error in g = theta/L
@@ -136,7 +135,7 @@ def track_entrance(d, P0c, L=0, theta = 0, g_err=0, e1=0, f_int = 0, h_gap = 0):
     return np.array([xf, pxf, yf, pyf, zf, pzf])
    
 
-def track_exit(b, P0c, L=0, theta=0, g_err=0, e2=0, f_int=0, h_gap=0):
+def track_exit(b, L=0, theta=0, g_err=0, e2=0, f_int=0, h_gap=0):
     """
     Tracks a 6-D beam through the exit fringe of a bending magnet.
     See chapter 16.2 of the Bmad manual.
@@ -144,7 +143,6 @@ def track_exit(b, P0c, L=0, theta=0, g_err=0, e2=0, f_int=0, h_gap=0):
     
     Input:
         b: initial 6D bmad beam coord
-        P0c: reference momentum in MeV/c
         L: length to track in m
         theta: bending angle 
         g_err: error in g = theta/L
@@ -205,6 +203,7 @@ def track_a_drift(b, P0c, L=0):
     
     Input:
         b: initial 6D bmad beam coord
+        P0c: reference momentum in eV/c
         L: length to track in m
 
     Ouptut:
@@ -219,9 +218,9 @@ def track_a_drift(b, P0c, L=0):
     
     pl = np.sqrt(1-(px**2 + py**2)/(1 + pz**2)) # unitless
     
-    P = P0c*(1 + pz) # in MeV/c
-    E = np.sqrt(P**2 + 0.510998950**2) # in MeV 
-    E0 = np.sqrt(P0c**2 + 0.510998950**2) # in MeV 
+    P = P0c*(1 + pz) # in eV
+    E = np.sqrt(P**2 + 510998.950**2) # in eV 
+    E0 = np.sqrt(P0c**2 + 510998.950**2) # in eV 
     beta = P/E
     beta0 = P0c/E0
     

gathering/csr_kick.py
@@ -6,7 +6,7 @@ from scipy.signal import convolve2d, fftconvolve, oaconvolve
 from cupyx.scipy.ndimage import convolve as cupy_conv
 
 
-from csr2d.deposit import split_particles, deposit_particles
+from csr2d.deposit import split_particles, deposit_particles, histogram_cic_2d
 from csr2d.central_difference import central_difference_z
 from csr2d.core import psi_s, psi_x
 #from csr2d.beam_conversion import particle_group_to_bmad, bmad_to_particle_group
@@ -18,31 +18,34 @@ import concurrent.futures as cf
 from scipy.signal import savgol_filter
 from scipy.interpolate import RectBivariateSpline
 
-def calc_csr_kick(beam, charges, Np, gamma, rho, Nz=100, Nx=100, verbose=True):
+def calc_csr_kick(beam, charges, Np, gamma, rho, Nz=100, sigma_z=1E-3, Nx=100, sigma_x=1E-3, reuse_psi_grids=False, psi_s_grid_old=None, psi_x_grid_old=None, verbose=True):
     """
-    
     """
     (x_b, xp_b, y_b, yp_b, z_b, zp_b) = beam
     zx_positions = np.stack((z_b, x_b)).T
     
     # Fix the grid here
-    # The grid needs to enclose all particles in z-x space  
-    mins = np.array([-51E-6,-51E-6])  # Lower bounds of the grid
-    maxs = np.array([51E-6,51E-6])     # Upper bounds of the grid
+    # The grid needs to enclose most particles in z-x space  
+    mins = np.array([-6*sigma_z,-6*sigma_x])  # Lower bounds of the grid
+    maxs = np.array([ 6*sigma_z, 6*sigma_x])     # Upper bounds of the grid
     sizes = np.array([Nz, Nx])
 
     (zmin,xmin) = mins
     (zmax,xmax) = maxs
-    #(Nz,Nx) = sizes
+    (Nz,Nx) = sizes
     (dz,dx) = (maxs - mins)/(sizes-1)  # grid steps
     
     
-    indexes, contrib = split_particles(zx_positions, charges, mins, maxs, sizes)
+    #indexes, contrib = split_particles(zx_positions, charges, mins, maxs, sizes)
+    #t1 = time.time();
+    #charge_grid = deposit_particles(Np, sizes, indexes, contrib)
+    #t2 = time.time();
+    
     t1 = time.time();
-    charge_grid = deposit_particles(Np, sizes, indexes, contrib)
+    charge_grid = histogram_cic_2d( z_b, x_b, charges, Nz, zmin, zmax, Nx, xmin, xmax )
     t2 = time.time();
     
-
+    
         
     # Normalize the grid so its integral is unity
     norm = np.sum(charge_grid)*dz*dx
@@ -62,19 +65,24 @@ def calc_csr_kick(beam, charges, Np, gamma, rho, Nz=100, Nx=100, verbose=True):
     
     
     t3 = time.time()
-
-    # Creating the grid of the potential 
-    zvec2 = np.linspace(2*zmin,2*zmax,2*Nz)
-    xvec2 = np.linspace(2*xmin,2*xmax,2*Nx)
-    zm2, xm2 = np.meshgrid(zvec2, xvec2, indexing='ij')
-    #psi_s_grid = psi_s(zm2,xm2,beta)
     
-    beta_grid = beta*np.ones(zm2.shape)
-    with cf.ProcessPoolExecutor(max_workers = 12) as executor:
-        temp = executor.map(psi_s, zm2, xm2, beta_grid)
-        psi_s_grid = np.array(list(temp))
-        temp2 = executor.map(psi_x, zm2, xm2, beta_grid)
-        psi_x_grid = np.array(list(temp2))
+    if (reuse_psi_grids == True):
+        psi_s_grid = psi_s_grid_old
+        psi_x_grid = psi_x_grid_old
+        
+    else:
+        # Creating the potential grids 
+        zvec2 = np.linspace(2*zmin,2*zmax,2*Nz)
+        xvec2 = np.linspace(2*xmin,2*xmax,2*Nx)
+        zm2, xm2 = np.meshgrid(zvec2, xvec2, indexing='ij')
+        #psi_s_grid = psi_s(zm2,xm2,beta)
+    
+        beta_grid = beta*np.ones(zm2.shape)
+        with cf.ProcessPoolExecutor(max_workers = 12) as executor:
+            temp = executor.map(psi_s, zm2/2/rho, xm2, beta_grid)
+            psi_s_grid = np.array(list(temp))
+            temp2 = executor.map(psi_x, zm2/2/rho, xm2, beta_grid)
+            psi_x_grid = np.array(list(temp2))
     
     t4 = time.time();
 
@@ -94,13 +102,17 @@ def calc_csr_kick(beam, charges, Np, gamma, rho, Nz=100, Nx=100, verbose=True):
     Ws_interp = RectBivariateSpline(zvec, xvec, Ws_grid)
     Wx_interp = RectBivariateSpline(zvec, xvec, Wx_grid)
 
-    
+
     r_e = 2.8179403227E-15 
-    Nb = np.sum(contrib)/1.602176634E-19
+    q_e = 1.602176634E-19
+    Nb =  np.sum(charge_grid)/q_e
     kick_factor = r_e*Nb/gamma
     
     # Calculate the kicks at the paritcle locations
     delta_kick = kick_factor * Ws_interp.ev(z_b, x_b)
     xp_kick    = kick_factor * Wx_interp.ev(z_b, x_b)
     
-    return delta_kick, xp_kick
+    
+    return {'zvec':zvec, 'xvec':xvec, 'delta_kick':delta_kick, 'xp_kick':xp_kick, 'Ws_grid':Ws_grid, 'Wx_grid':Wx_grid, 'psi_s_grid':psi_s_grid, 'psi_x_grid':psi_x_grid}
+    
+    #return delta_kick, xp_kick