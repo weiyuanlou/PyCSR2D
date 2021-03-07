cd2b2451d52bee8f5bcc0749c21846ced4a2c3ba
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2021-03-06 17:28:12 -0800

    1D CSR steady state

--- csr2d/kick.py
+++ csr2d/kick.py
@@ -200,3 +200,87 @@ def csr2d_kick_calc(z_b, x_b, weight,
         result.update({'zvec':zvec, 'xvec':xvec, 'Ws_grid':Ws_grid, 'Wx_grid':Wx_grid, 'psi_s_grid':psi_s_grid, 'psi_x_grid':psi_x_grid, 'charge_grid':charge_grid})
     
     return result
+
+
+
+
+
+
+
+def csr1d_steady_state_kick_calc(z, weights,
+                    *,
+                    nz = 100,
+                    rho=1,
+                    species='electron'):
+    
+    """
+
+    Steady State CSR 1D model kick calc
+
+    
+    Parameters
+    ----------
+    z : np.array
+        Bunch z coordinates in [m]    
+        
+    weights : np.array
+        weight array (positive only) in [C]
+        This should sum to the total charge in the bunch        
+        
+    nz : int
+        number of z grid points        
+        
+    rho : float
+        bending radius in [m]        
+        
+    species : str
+        Particle species. Currently required to be 'electron'   
+        
+    Returns
+    -------
+    dict with:
+    
+        denergy_ds : np.array
+            energy kick for each particle [eV/m]
+            
+        wake : np.array
+            wake array that kicks were interpolated on
+            
+        zvec : np.array
+            z coordinates for wake array
+    
+    """
+    
+
+    assert species == 'electron', f'TODO: support species {species}'
+    
+    # Density
+    H, edges = np.histogram(z, weights=weights, bins=nz)
+    zmin, zmax = edges[0], edges[-1]
+    dz = (zmax-zmin)/(nz-1)
+    
+    zvec = np.linspace(zmin, zmax, nz) # Sloppy with bin centers
+    
+    Qtot=np.sum(weights)
+    density = H/dz/Qtot
+    
+    # Density derivative
+    densityp = np.gradient(density)/dz
+    densityp_filtered = savgol_filter( densityp, 13, 2 )
+    
+    # Green function
+    zi = np.arange(0, zmax-zmin, dz)
+    factor = -3**(2/3) * Qtot/e_charge * r_e * mec2 * rho**(-2/3)  # factor for denergy/dz [eV/m]
+    #factor = -3**(2/3) * Qtot/e_charge * r_e * rho**(-2/3) / gamma  # factor for ddelta/ds [1/m]
+    green = factor*np.diff(zi**(2/3))
+    
+    # Convolve to get wake
+    wake = np.convolve(densityp_filtered, green, mode='full')[0:len(zvec)]
+    
+    # Interpolate to get the kicks
+    delta_kick = np.interp(z, zvec, wake)
+    
+    return {'denergy_ds':delta_kick,
+             'zvec':zvec, 'wake':wake
+             }
+
