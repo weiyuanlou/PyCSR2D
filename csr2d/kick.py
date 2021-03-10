ed6d48bf16286b393ceef7e20bd3ec14051eb5e0
weiyuanlou <wlou@slac.stanford.edu>
2021-03-10 12:52:38 -0800

    updated kick.py to allow negative bending angle. Updated chicane benchmarking

--- csr2d/kick.py
+++ csr2d/kick.py
@@ -9,28 +9,33 @@ from scipy.interpolate import RectBivariateSpline
 from scipy.signal import convolve2d, fftconvolve, oaconvolve
 
 import scipy.constants
-mec2 = scipy.constants.value('electron mass energy equivalent in MeV')*1e6
+
+mec2 = scipy.constants.value("electron mass energy equivalent in MeV") * 1e6
 c_light = scipy.constants.c
 e_charge = scipy.constants.e
-r_e = scipy.constants.value('classical electron radius')
+r_e = scipy.constants.value("classical electron radius")
 
 import time
 
 
-def csr2d_kick_calc(z_b, x_b, weight,
-                    *, 
-                    gamma=None,
-                    rho=None,
-                    nz=100,
-                    nx=100,
-                    xlim=None,
-                    zlim=None,
-                    reuse_psi_grids=False,
-                    psi_s_grid_old=None,
-                    psi_x_grid_old=None,
-                    map_f=map,
-                    species='electron',
-                    debug=False):
+def csr2d_kick_calc(
+    z_b,
+    x_b,
+    weight,
+    *,
+    gamma=None,
+    rho=None,
+    nz=100,
+    nx=100,
+    xlim=None,
+    zlim=None,
+    reuse_psi_grids=False,
+    psi_s_grid_old=None,
+    psi_x_grid_old=None,
+    map_f=map,
+    species="electron",
+    debug=False,
+):
     """
     
     Calculates the 2D CSR kick on a set of particles with positions `z_b`, `x_b` and charges `charges`.
@@ -53,6 +58,7 @@ def csr2d_kick_calc(z_b, x_b, weight,
         
     rho : float
         bending radius in [m]
+        if neagtive, particles with a positive x coordinate is on the inner side of the magnet
         
     nz : int
         number of z grid points
@@ -92,10 +98,14 @@ def csr2d_kick_calc(z_b, x_b, weight,
     
         
     """
-    assert species == 'electron', 'TODO: support species {species}'
-    assert np.sign(rho) == 1, 'TODO: negative rho'
-    
-    
+    assert species == "electron", "TODO: support species {species}"
+    # assert np.sign(rho) == 1, 'TODO: negative rho'
+
+    rho_sign = np.sign(rho)
+    if rho_sign == -1:
+        rho = -rho
+        x_b = -x_b  # flip the beam x coordinate
+
     # Grid setup
     if zlim:
         zmin = zlim[0]
@@ -109,110 +119,119 @@ def csr2d_kick_calc(z_b, x_b, weight,
         xmax = xlim[1]
     else:
         xmin = x_b.min()
-        xmax = x_b.max()        
-    
-    dz = (zmax-zmin)/(nz-1) 
-    dx = (xmax-xmin)/(nx-1)  
-    
+        xmax = x_b.max()
+
+    dz = (zmax - zmin) / (nz - 1)
+    dx = (xmax - xmin) / (nx - 1)
+
     # Charge deposition
-    
+
     # Old method
-    #zx_positions = np.stack((z_b, x_b)).T
-    #indexes, contrib = split_particles(zx_positions, charges, mins, maxs, sizes)
-    #t1 = time.time();
-    #charge_grid = deposit_particles(Np, sizes, indexes, contrib)
-    #t2 = time.time();
-    
-    # Remi's fast code 
-    t1 = time.time();
-    charge_grid = histogram_cic_2d( z_b, x_b, weight, nz, zmin, zmax, nx, xmin, xmax )
-    
+    # zx_positions = np.stack((z_b, x_b)).T
+    # indexes, contrib = split_particles(zx_positions, charges, mins, maxs, sizes)
+    # t1 = time.time();
+    # charge_grid = deposit_particles(Np, sizes, indexes, contrib)
+    # t2 = time.time();
+
+    # Remi's fast code
+    t1 = time.time()
+    charge_grid = histogram_cic_2d(z_b, x_b, weight, nz, zmin, zmax, nx, xmin, xmax)
+
     if debug:
-        t2 = time.time();
-        print('Depositing particles takes:', t2 - t1, 's')    
-        
+        t2 = time.time()
+        print("Depositing particles takes:", t2 - t1, "s")
+
     # Normalize the grid so its integral is unity
-    norm = np.sum(charge_grid)*dz*dx
-    lambda_grid = charge_grid/norm
-    
+    norm = np.sum(charge_grid) * dz * dx
+    lambda_grid = charge_grid / norm
+
     # Apply savgol filter
-    lambda_grid_filtered = np.array([savgol_filter( lambda_grid[:,i], 13, 2 ) for i in np.arange(nx)]).T
-    
+    lambda_grid_filtered = np.array(
+        [savgol_filter(lambda_grid[:, i], 13, 2) for i in np.arange(nx)]
+    ).T
+
     # Differentiation in z
-    lambda_grid_filtered_prime = central_difference_z(lambda_grid_filtered, nz, nx, dz, order=1)
-    
-    # Grid axis vectors 
+    lambda_grid_filtered_prime = central_difference_z(
+        lambda_grid_filtered, nz, nx, dz, order=1
+    )
+
+    # Grid axis vectors
     zvec = np.linspace(zmin, zmax, nz)
     xvec = np.linspace(xmin, xmax, nx)
-    
-    beta = np.sqrt(1-1/gamma**2)
-    
+
+    beta = np.sqrt(1 - 1 / gamma ** 2)
+
     t3 = time.time()
-    
-    if (reuse_psi_grids == True):
+
+    if reuse_psi_grids == True:
         psi_s_grid = psi_s_grid_old
         psi_x_grid = psi_x_grid_old
-        
-    else:
-        # Creating the potential grids 
-        zvec2 = np.linspace(2*zmin,2*zmax,2*nz)
-        xvec2 = np.linspace(2*xmin,2*xmax,2*nx)
-        zm2, xm2 = np.meshgrid(zvec2, xvec2, indexing='ij')
 
-        beta_grid = beta*np.ones(zm2.shape)
-        
+    else:
+        # Creating the potential grids
+        zvec2 = np.linspace(2 * zmin, 2 * zmax, 2 * nz)
+        xvec2 = np.linspace(2 * xmin, 2 * xmax, 2 * nx)
+        zm2, xm2 = np.meshgrid(zvec2, xvec2, indexing="ij")
+
+        beta_grid = beta * np.ones(zm2.shape)
+
         # Map (possibly parallel)
-        temp = map_f(psi_s, zm2/2/rho, xm2, beta_grid)
+        temp = map_f(psi_s, zm2 / 2 / rho, xm2, beta_grid)
         psi_s_grid = np.array(list(temp))
-        temp2 = map_f(psi_x, zm2/2/rho, xm2, beta_grid)
+        temp2 = map_f(psi_x, zm2 / 2 / rho, xm2, beta_grid)
         psi_x_grid = np.array(list(temp2))
-    
+
     if debug:
-        t4 = time.time();
-        print('Computing potential grids take:', t4 - t3, 's')
-        
+        t4 = time.time()
+        print("Computing potential grids take:", t4 - t3, "s")
+
     # Compute the wake via 2d convolution
-    conv_s = oaconvolve(lambda_grid_filtered_prime, psi_s_grid, mode='same')
-    conv_x = oaconvolve(lambda_grid_filtered_prime, psi_x_grid, mode='same')
-    
+    conv_s = oaconvolve(lambda_grid_filtered_prime, psi_s_grid, mode="same")
+    conv_x = oaconvolve(lambda_grid_filtered_prime, psi_x_grid, mode="same")
+
     if debug:
         t5 = time.time()
-        print('Convolution takes:', t5 - t4, 's')    
-    
-    Ws_grid = (beta**2/rho)*(conv_s)*(dz*dx)
-    Wx_grid = (beta**2/rho)*(conv_x)*(dz*dx)
-    
+        print("Convolution takes:", t5 - t4, "s")
+
+    Ws_grid = (beta ** 2 / rho) * (conv_s) * (dz * dx)
+    Wx_grid = (beta ** 2 / rho) * (conv_x) * (dz * dx)
+
     # Interpolate Ws and Wx everywhere within the grid
     Ws_interp = RectBivariateSpline(zvec, xvec, Ws_grid)
     Wx_interp = RectBivariateSpline(zvec, xvec, Wx_grid)
 
     # Overall factor
-    Nb =  np.sum(weight)/e_charge
-    kick_factor = r_e*Nb/gamma # m
-    
+    Nb = np.sum(weight) / e_charge
+    kick_factor = r_e * Nb / gamma  # m
+
     # Calculate the kicks at the particle locations
     delta_kick = kick_factor * Ws_interp.ev(z_b, x_b)
-    xp_kick    = kick_factor * Wx_interp.ev(z_b, x_b)
-    
-    result = {'ddelta_ds':delta_kick, 'dxp_ds':xp_kick}
-    
+    xp_kick = kick_factor * Wx_interp.ev(z_b, x_b)
+
+    if rho_sign == -1:
+        xp_kick = -xp_kick
+
+    result = {"ddelta_ds": delta_kick, "dxp_ds": xp_kick}
+
     if debug:
-        result.update({'zvec':zvec, 'xvec':xvec, 'Ws_grid':Ws_grid, 'Wx_grid':Wx_grid, 'psi_s_grid':psi_s_grid, 'psi_x_grid':psi_x_grid, 'charge_grid':charge_grid})
-    
+        result.update(
+            {
+                "zvec": zvec,
+                "xvec": xvec,
+                "Ws_grid": Ws_grid,
+                "Wx_grid": Wx_grid,
+                "psi_s_grid": psi_s_grid,
+                "psi_x_grid": psi_x_grid,
+                "charge_grid": charge_grid,
+                "lambda_grid_filtered_prime": lambda_grid_filtered_prime,
+            }
+        )
+
     return result
 
 
+def csr1d_steady_state_kick_calc(z, weights, *, nz=100, rho=1, species="electron"):
 
-
-
-
-
-def csr1d_steady_state_kick_calc(z, weights,
-                    *,
-                    nz = 100,
-                    rho=1,
-                    species='electron'):
-    
     """
 
     Steady State CSR 1D model kick calc
@@ -250,37 +269,35 @@ def csr1d_steady_state_kick_calc(z, weights,
             z coordinates for wake array
     
     """
-    
 
-    assert species == 'electron', f'TODO: support species {species}'
-    
+    assert species == "electron", f"TODO: support species {species}"
+
     # Density
     H, edges = np.histogram(z, weights=weights, bins=nz)
     zmin, zmax = edges[0], edges[-1]
-    dz = (zmax-zmin)/(nz-1)
-    
-    zvec = np.linspace(zmin, zmax, nz) # Sloppy with bin centers
-    
-    Qtot=np.sum(weights)
-    density = H/dz/Qtot
-    
+    dz = (zmax - zmin) / (nz - 1)
+
+    zvec = np.linspace(zmin, zmax, nz)  # Sloppy with bin centers
+
+    Qtot = np.sum(weights)
+    density = H / dz / Qtot
+
     # Density derivative
-    densityp = np.gradient(density)/dz
-    densityp_filtered = savgol_filter( densityp, 13, 2 )
-    
+    densityp = np.gradient(density) / dz
+    densityp_filtered = savgol_filter(densityp, 13, 2)
+
     # Green function
-    zi = np.arange(0, zmax-zmin, dz)
-    factor = -3**(2/3) * Qtot/e_charge * r_e * mec2 * rho**(-2/3)  # factor for denergy/dz [eV/m]
-    #factor = -3**(2/3) * Qtot/e_charge * r_e * rho**(-2/3) / gamma  # factor for ddelta/ds [1/m]
-    green = factor*np.diff(zi**(2/3))
-    
+    zi = np.arange(0, zmax - zmin, dz)
+    factor = (
+        -3 ** (2 / 3) * Qtot / e_charge * r_e * mec2 * rho ** (-2 / 3)
+    )  # factor for denergy/dz [eV/m]
+    # factor = -3**(2/3) * Qtot/e_charge * r_e * rho**(-2/3) / gamma  # factor for ddelta/ds [1/m]
+    green = factor * np.diff(zi ** (2 / 3))
+
     # Convolve to get wake
-    wake = np.convolve(densityp_filtered, green, mode='full')[0:len(zvec)]
-    
+    wake = np.convolve(densityp_filtered, green, mode="full")[0 : len(zvec)]
+
     # Interpolate to get the kicks
     delta_kick = np.interp(z, zvec, wake)
-    
-    return {'denergy_ds':delta_kick,
-             'zvec':zvec, 'wake':wake
-             }
 
+    return {"denergy_ds": delta_kick, "zvec": zvec, "wake": wake}
--- csr2d/simple_track.py
+++ csr2d/simple_track.py
@@ -1,23 +1,26 @@
-import numpy as np 
+import numpy as np
+
 
 def sinc(x):
     return np.sinc(x / np.pi)
+
 
 @np.vectorize
 def cosc(x):
     if x == 0:
         return -0.5
     else:
-        return (np.cos(x) - 1) / x**2
+        return (np.cos(x) - 1) / x ** 2
 
-def track_a_bend(b, P0c, L=0, theta = 0, g_err=0):
+
+def track_a_bend(b, p0c, L=0, theta=0, g_err=0):
     """
     Tracks a 6-D beam through a bending magnet.
     See chapter 23.6 of the Bmad manual.
     
     Input:
         b: initial 6D bmad beam coord
-        P0c: reference momentum in eV/c
+        p0c: reference momentum in eV/c
         L: length to track in m
         theta: bending angle 
         g_err: error in g = theta/L
@@ -25,57 +28,61 @@ def track_a_bend(b, P0c, L=0, theta = 0, g_err=0):
     Ouptut:
         d: final 6D bmad beam coord
     """
-    x  = b[0]
+    x = b[0]
     px = b[1]
-    y  = b[2]
+    y = b[2]
     py = b[3]
-    z  = b[4]
+    z = b[4]
     pz = b[5]
-    
-    px_norm = np.sqrt((1 + pz)**2 - py**2)  # For simplicity
-    
+
+    px_norm = np.sqrt((1 + pz) ** 2 - py ** 2)  # For simplicity
+
     phi1 = np.arcsin(px / px_norm)
 
-    g = theta / L 
+    g = theta / L
     g_tot = g + g_err
     gp = g_tot / px_norm
-    
-    alpha = 2*(1 + g*x) *np.sin(theta + phi1) *L*sinc(theta) \
-            - gp*((1 + g*x)*L*sinc(theta))**2
-    
-    x2_t1 = x*np.cos(theta) + L**2 *g*cosc(theta) 
-    x2_t2 = np.sqrt((np.cos(theta + phi1)**2) + gp*alpha)
-    x2_t3 = np.cos(theta + phi1)
-        
-    x2 = np.where(np.abs(theta + phi1) < np.pi/2, 
-                  (x2_t1 + alpha / (x2_t2 + x2_t3)), 
-                  (x2_t1 + (x2_t2 - x2_t3) / gp))
-        
-    Lcu = x2 - L**2 *g*cosc(theta) - x*np.cos(theta)
-    Lcv = -L*sinc(theta) - x*np.sin(theta)
 
-    theta_p = 2*(theta + phi1 - np.pi/2 - np.arctan2(Lcv, Lcu))
-    
-    Lc = np.sqrt(Lcu**2 + Lcv**2)
+    alpha = (
+        2 * (1 + g * x) * np.sin(theta + phi1) * L * sinc(theta)
+        - gp * ((1 + g * x) * L * sinc(theta)) ** 2
+    )
+
+    x2_t1 = x * np.cos(theta) + L ** 2 * g * cosc(theta)
+    x2_t2 = np.sqrt((np.cos(theta + phi1) ** 2) + gp * alpha)
+    x2_t3 = np.cos(theta + phi1)
+
+    x2 = np.where(
+        np.abs(theta + phi1) < np.pi / 2,
+        (x2_t1 + alpha / (x2_t2 + x2_t3)),
+        (x2_t1 + (x2_t2 - x2_t3) / gp),
+    )
+
+    Lcu = x2 - L ** 2 * g * cosc(theta) - x * np.cos(theta)
+    Lcv = -L * sinc(theta) - x * np.sin(theta)
+
+    theta_p = 2 * (theta + phi1 - np.pi / 2 - np.arctan2(Lcv, Lcu))
+
+    Lc = np.sqrt(Lcu ** 2 + Lcv ** 2)
     Lp = Lc / sinc(theta_p / 2)
-    
-    P = P0c*(1 + pz) # in eV
-    E = np.sqrt(P**2 + 510998.950**2) # in eV 
-    E0 = np.sqrt(P0c**2 + 510998.950**2) # in eV 
-    beta = P/E
-    beta0 = P0c/E0
-    
-    xf  = x2
-    pxf = px_norm * np.sin( theta + phi1 - theta_p )
-    yf  = y + py*Lp / px_norm 
+
+    P = p0c * (1 + pz)  # in eV
+    E = np.sqrt(P ** 2 + 510998.950 ** 2)  # in eV
+    E0 = np.sqrt(p0c ** 2 + 510998.950 ** 2)  # in eV
+    beta = P / E
+    beta0 = p0c / E0
+
+    xf = x2
+    pxf = px_norm * np.sin(theta + phi1 - theta_p)
+    yf = y + py * Lp / px_norm
     pyf = py
-    zf  = z + ( beta*L / beta0 ) - ((1+pz)*Lp / px_norm)
+    zf = z + (beta * L / beta0) - ((1 + pz) * Lp / px_norm)
     pzf = pz
-    
+
     return np.array([xf, pxf, yf, pyf, zf, pzf])
-    
-    
-def track_entrance(d, L=0, theta = 0, g_err=0, e1=0, f_int = 0, h_gap = 0):
+
+
+def track_entrance(d, L=0, theta=0, g_err=0, e1=0, f_int=0, h_gap=0):
     """
     Tracks a 6-D beam through the entrance fringe of a bending magnet.
     See chapter 16.2 of the Bmad manual.
@@ -90,48 +97,58 @@ def track_entrance(d, L=0, theta = 0, g_err=0, e1=0, f_int = 0, h_gap = 0):
     Ouptut:
         d: final 6D bmad beam coord
     """
-    x  = b[0]
+    x = b[0]
     px = b[1]
-    y  = b[2]
+    y = b[2]
     py = b[3]
-    z  = b[4]
+    z = b[4]
     pz = b[5]
 
-    g = theta / L   
+    g = theta / L
     g_tot = g + g_err
-    
-    sin = np.sin(e1) 
-    tan = np.tan(e1) 
-    sec = 1/np.cos(e1)
-    
-    Sigma_M1 = (x**2 - y**2)* g_tot * tan / 2  \
-             + y**2 * g_tot**2 * sec**3 * ( 1 + sin**2 ) * f_int * h_gap / 2 / (1+pz)  \
-             - x**3 * g_tot**2 * tan**3 / 12 / (1+pz)  \
-             + x * y**2 * g_tot**2 * tan * sec**2 / 4 / (1+pz)  \
-             + (x**2 * px - 2*x*y*py) * g_tot * tan**2 / 2 / (1+pz)  \
-             - y**2 * px * g_tot * (1 + tan**2) / 2 / (1+pz)
-            
-    xf  = x + g_tot/2/(1+pz)*( - x**2*tan**2 + y**2*sec**2 )
-    
-    pxf = px  \
-        + x*g_tot*tan  \
-        + y**2 * g_tot**2 * ( tan + 2*tan**3 )/2/(1+pz)  \
-        + (x*px - y*py)*g_tot*tan**2/(1+pz)
-    
-    yf  = y + x*y*g_tot * tan**2 / (1+pz)
-    
-    pyf = py \
-        + y*( -g_tot*tan + g_tot**2*(1+sin**2)*sec**3 * f_int * h_gap/(1+pz) ) \
-        + x*y*g_tot**2*sec**2*tan/(1+pz)  \
-        - x*py*g_tot*tan**2/(1+pz)  \
-        - y*px*g_tot*(1+tan**2)/(1+pz)
-    
-    zf  = z + ( Sigma_M1 - (x**2 - y**2)*g_tot*tan/2 ) / (1+pz) 
-    
+
+    sin = np.sin(e1)
+    tan = np.tan(e1)
+    sec = 1 / np.cos(e1)
+
+    Sigma_M1 = (
+        (x ** 2 - y ** 2) * g_tot * tan / 2
+        + y ** 2 * g_tot ** 2 * sec ** 3 * (1 + sin ** 2) * f_int * h_gap / 2 / (1 + pz)
+        - x ** 3 * g_tot ** 2 * tan ** 3 / 12 / (1 + pz)
+        + x * y ** 2 * g_tot ** 2 * tan * sec ** 2 / 4 / (1 + pz)
+        + (x ** 2 * px - 2 * x * y * py) * g_tot * tan ** 2 / 2 / (1 + pz)
+        - y ** 2 * px * g_tot * (1 + tan ** 2) / 2 / (1 + pz)
+    )
+
+    xf = x + g_tot / 2 / (1 + pz) * (-x ** 2 * tan ** 2 + y ** 2 * sec ** 2)
+
+    pxf = (
+        px
+        + x * g_tot * tan
+        + y ** 2 * g_tot ** 2 * (tan + 2 * tan ** 3) / 2 / (1 + pz)
+        + (x * px - y * py) * g_tot * tan ** 2 / (1 + pz)
+    )
+
+    yf = y + x * y * g_tot * tan ** 2 / (1 + pz)
+
+    pyf = (
+        py
+        + y
+        * (
+            -g_tot * tan
+            + g_tot ** 2 * (1 + sin ** 2) * sec ** 3 * f_int * h_gap / (1 + pz)
+        )
+        + x * y * g_tot ** 2 * sec ** 2 * tan / (1 + pz)
+        - x * py * g_tot * tan ** 2 / (1 + pz)
+        - y * px * g_tot * (1 + tan ** 2) / (1 + pz)
+    )
+
+    zf = z + (Sigma_M1 - (x ** 2 - y ** 2) * g_tot * tan / 2) / (1 + pz)
+
     pzf = pz
-    
+
     return np.array([xf, pxf, yf, pyf, zf, pzf])
-   
+
 
 def track_exit(b, L=0, theta=0, g_err=0, e2=0, f_int=0, h_gap=0):
     """
@@ -148,51 +165,62 @@ def track_exit(b, L=0, theta=0, g_err=0, e2=0, f_int=0, h_gap=0):
     Ouptut:
         d: final 6D bmad beam coord
     """
-    x  = b[0]
+    x = b[0]
     px = b[1]
-    y  = b[2]
+    y = b[2]
     py = b[3]
-    z  = b[4]
+    z = b[4]
     pz = b[5]
 
-    g = theta / L   
+    g = theta / L
     g_tot = g + g_err
     print("g_tot:", g_tot)
-    
-    sin = np.sin(e2) 
-    tan = np.tan(e2) 
-    sec = 1/np.cos(e2)
-    
-    # The 5th and 6th term have the opposite sign from Sigma_M1
-    Sigma_M2 = (x**2 - y**2)* g_tot * tan / 2  \
-             + y**2 * g_tot**2 * sec**3 * ( 1 + sin**2 ) * f_int * h_gap / 2 / (1+pz)  \
-             - x**3 * g_tot**2 * tan**3 / 12 / (1+pz)  \
-             + x * y**2 * g_tot**2 * tan * sec**2 / 4 / (1+pz)  \
-             + (-x**2*px + 2*x*y*py) * g_tot * tan**2 / 2 / (1+pz)  \
-             + y**2 * px * g_tot * (1 + tan**2) / 2 / (1+pz)
 
-    xf  = x + g_tot/2/(1 + pz)*( x**2*tan**2 - y**2*sec**2 )
-    
-    pxf = px \
-        + x * g_tot * tan  \
-        - (x**2 + y**2) *g_tot**2 * tan**3/2/(1+pz) \
-        + (-x*px + y*py)*g_tot*tan**2/(1+pz)
-    
-    yf  = y - x*y*g_tot * tan**2 / (1+pz)
-    
-    pyf = py \
-        + y*( -g_tot*tan + g_tot**2*(1+sin**2)*sec**3 * f_int * h_gap/(1+pz) ) \
-        + x*y*g_tot**2*sec**2*tan/(1+pz)  \
-        + x*py*g_tot*tan**2/(1+pz)  \
-        + y*px*g_tot*(1+tan**2)/(1+pz)
-    
-    zf  = z + ( Sigma_M2 - (x**2 - y**2)*g_tot*tan/2 ) / (1+pz) 
-    
+    sin = np.sin(e2)
+    tan = np.tan(e2)
+    sec = 1 / np.cos(e2)
+
+    # The 5th and 6th term have the opposite sign from Sigma_M1
+    Sigma_M2 = (
+        (x ** 2 - y ** 2) * g_tot * tan / 2
+        + y ** 2 * g_tot ** 2 * sec ** 3 * (1 + sin ** 2) * f_int * h_gap / 2 / (1 + pz)
+        - x ** 3 * g_tot ** 2 * tan ** 3 / 12 / (1 + pz)
+        + x * y ** 2 * g_tot ** 2 * tan * sec ** 2 / 4 / (1 + pz)
+        + (-x ** 2 * px + 2 * x * y * py) * g_tot * tan ** 2 / 2 / (1 + pz)
+        + y ** 2 * px * g_tot * (1 + tan ** 2) / 2 / (1 + pz)
+    )
+
+    xf = x + g_tot / 2 / (1 + pz) * (x ** 2 * tan ** 2 - y ** 2 * sec ** 2)
+
+    pxf = (
+        px
+        + x * g_tot * tan
+        - (x ** 2 + y ** 2) * g_tot ** 2 * tan ** 3 / 2 / (1 + pz)
+        + (-x * px + y * py) * g_tot * tan ** 2 / (1 + pz)
+    )
+
+    yf = y - x * y * g_tot * tan ** 2 / (1 + pz)
+
+    pyf = (
+        py
+        + y
+        * (
+            -g_tot * tan
+            + g_tot ** 2 * (1 + sin ** 2) * sec ** 3 * f_int * h_gap / (1 + pz)
+        )
+        + x * y * g_tot ** 2 * sec ** 2 * tan / (1 + pz)
+        + x * py * g_tot * tan ** 2 / (1 + pz)
+        + y * px * g_tot * (1 + tan ** 2) / (1 + pz)
+    )
+
+    zf = z + (Sigma_M2 - (x ** 2 - y ** 2) * g_tot * tan / 2) / (1 + pz)
+
     pzf = pz
-    
+
     return np.array([xf, pxf, yf, pyf, zf, pzf])
 
-def track_a_drift(b, P0c, L=0):
+
+def track_a_drift(b, p0c, L=0):
     """
     Tracks a 6-D beam through a drift.
     See chapter 23.7 of the Bmad manual.
@@ -200,34 +228,32 @@ def track_a_drift(b, P0c, L=0):
     
     Input:
         b: initial 6D bmad beam coord
-        P0c: reference momentum in eV/c
+        p0c: reference momentum in eV/c
         L: length to track in m
 
     Ouptut:
         d: final 6D bmad beam coord
     """
-    x  = b[0]
+    x = b[0]
     px = b[1]
-    y  = b[2]
+    y = b[2]
     py = b[3]
-    z  = b[4]
+    z = b[4]
     pz = b[5]
-    
-    pl = np.sqrt(1-(px**2 + py**2)/(1 + pz**2)) # unitless
-    
-    P = P0c*(1 + pz) # in eV
-    E = np.sqrt(P**2 + 510998.950**2) # in eV 
-    E0 = np.sqrt(P0c**2 + 510998.950**2) # in eV 
-    beta = P/E
-    beta0 = P0c/E0
-    
-    xf  = x + L * px / (1 + pz) / pl
+
+    pl = np.sqrt(1 - (px ** 2 + py ** 2) / (1 + pz ** 2))  # unitless
+
+    P = p0c * (1 + pz)  # in eV
+    E = np.sqrt(P ** 2 + 510998.950 ** 2)  # in eV
+    E0 = np.sqrt(p0c ** 2 + 510998.950 ** 2)  # in eV
+    beta = P / E
+    beta0 = p0c / E0
+
+    xf = x + L * px / (1 + pz) / pl
     pxf = px
-    yf  = y + L * py / (1 + pz) / pl
+    yf = y + L * py / (1 + pz) / pl
     pyf = py
-    zf  = z + ( beta/beta0 - 1/pl ) * L
+    zf = z + (beta / beta0 - 1 / pl) * L
     pzf = pz
-    
+
     return np.array([xf, pxf, yf, pyf, zf, pzf])
-    
-    
