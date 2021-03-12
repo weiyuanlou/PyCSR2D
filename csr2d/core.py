c6d4bc1a8650394fea7839c6d6d8af34b2e8ea7b
weiyuanlou <wlou@slac.stanford.edu>
2021-03-11 19:22:21 -0800

    Update format and description in core.py

--- csr2d/core.py
+++ csr2d/core.py
@@ -13,9 +13,8 @@ def psi_s(z, x, beta):
     Note that 'x' here corresponds to 'chi = x / rho' in the paper.
     """
     try:
-        out = (cos(2 * alpha(z, x, beta)) - 1 / (1 + x)) / (
-            kappa(z, x, beta) - beta * (1 + x) * sin(2 * alpha(z, x, beta))
-        )
+        out = (cos(2 * alpha(z, x, beta)) - 1 / (1+x)) / (
+            kappa(z, x, beta) - beta * (1+x) * sin(2 * alpha(z, x, beta)))
     except ZeroDivisionError:
         out = 0
         # print(f"Oops!  ZeroDivisionError at (z,x)= ({z:5.2f},{x:5.2f}). Returning 0.")
@@ -39,34 +38,18 @@ def ss_ellipe(phi, m):
 def psi_x(z, x, beta):
     """
     Eq.(24) from Ref[1] with argument zeta=0 and no constant factor e*beta**2/2/rho**2.
-    Note that 'x' here corresponds to 'chi = x / rho' in the paper.
+    Note that 'x' here corresponds to 'chi = x/rho', 
+    and 'z' here corresponds to 'xi = z/2/rho' in the paper. 
     """
     # z = np.float(z)
     # x = np.float(x)
     kap = kappa(z, x, beta)
     alp = alpha(z, x, beta)
-    arg2 = -4 * (1 + x) / x ** 2
+    arg2 = -4 * (1+x) / x**2
     try:
-        T1 = (
-            1
-            / abs(x)
-            / (1 + x)
-            * (
-                (2 + 2 * x + x ** 2) * ss.ellipkinc(alp, arg2)
-                - x ** 2 * ss.ellipeinc(alp, arg2)
-            )
-        )
-        D = kap ** 2 - beta ** 2 * (1 + x) ** 2 * sin(2 * alp) ** 2
-        T2 = (
-            (
-                kap ** 2
-                - 2 * beta ** 2 * (1 + x) ** 2
-                + beta ** 2 * (1 + x) * (2 + 2 * x + x ** 2) * cos(2 * alp)
-            )
-            / beta
-            / (1 + x)
-            / D
-        )
+        T1 = (1/abs(x)/(1 + x) * ((2 + 2*x + x**2) * ss.ellipkinc(alp, arg2)- x**2 * ss.ellipeinc(alp, arg2)))
+        D = kap**2 - beta**2 * (1 + x)**2 * sin(2*alp)**2
+        T2 = ((kap**2 - 2*beta** 2 * (1+x)**2 + beta**2 * (1+x) * (2 + 2*x + x**2) * cos(2*alp))/ beta/ (1+x)/ D)
         T3 = -kap * sin(2 * alp) / D
         T4 = kap * beta ** 2 * (1 + x) * sin(2 * alp) * cos(2 * alp) / D
         T5 = 1 / abs(x) * ss.ellipkinc(alp, arg2)  # psi_phi without e/rho**2 factor
@@ -80,82 +63,58 @@ def psi_x(z, x, beta):
 def nu(x, beta):
     """
     Eq. (6) from Ref[1] (coeffient of alpha**2)
-    Note that 'x' here corresponds to 'chi = x / rho' in the paper.
+    Note that 'x' here corresponds to 'chi = x/rho' in the paper.
     """
-    return 3 * (1 - beta ** 2 - beta ** 2 * x) / beta ** 2 / (1 + x)
+    return 3 * (1 - beta**2 - beta**2*x) / beta**2 / (1+x)
 
 
 def eta(z, x, beta):
     """
     Eq. (6) from Ref[1] (coeffient of alpha)
-    Note that 'x' here corresponds to 'chi = x / rho' in the paper.
+    Note that 'x' here corresponds to 'chi = x/rho', 
+    and 'z' here corresponds to 'xi = z/2/rho' in the paper. 
     """
-    return -6 * z / beta ** 2 / (1 + x)
+    return -6 * z / beta**2 / (1+x)
 
 
 def zeta(z, x, beta):
     """
     Eq. (6) from Ref[1] (constant term)
-    Note that 'x' here corresponds to 'chi = x / rho' in the paper.
+    Note that 'x' here corresponds to 'chi = x/rho', 
+    and 'z' here corresponds to 'xi = z/2/rho' in the paper. 
     """
-    return 3 * (4 * z ** 2 - beta ** 2 * x ** 2) / 4 / beta ** 2 / (1 + x)
+    return 3 * (4* z**2 - beta**2 * x**2) / 4 / beta**2 / (1+x)
 
 
 def Omega(z, x, beta):
     """
     Eq. (A3) from Ref[1]
-    Note that 'x' here corresponds to 'chi = x / rho' in the paper.
+    Note that 'x' here corresponds to 'chi = x/rho', 
+    and 'z' here corresponds to 'xi = z/2/rho' in the paper. 
     """
-    temp = (
-        eta(z, x, beta) ** 2 / 16
-        - zeta(z, x, beta) * nu(x, beta) / 6
-        + nu(x, beta) ** 3 / 216
-    )
-    return temp + (temp ** 2 - (zeta(z, x, beta) / 3 + nu(x, beta) ** 2 / 36) ** 3) ** (
-        1 / 2
-    )
+    temp = (eta(z, x, beta)**2/16
+        - zeta(z, x, beta) * nu(x, beta)/6
+        + nu(x, beta)**3/216)
+    return temp + (temp**2 - (zeta(z, x, beta)/3 + nu(x, beta)**2/36)**3)**(1/2)
 
 
 def m(z, x, beta):
     """
     Eq. (A2) from Ref[1]
-    Note that 'x' here corresponds to 'chi = x / rho' in the paper.
+    Note that 'x' here corresponds to 'chi = x/rho', 
+    and 'z' here corresponds to 'xi = z/2/rho' in the paper. 
     """
-    return (
-        -nu(x, beta) / 3
-        + (zeta(z, x, beta) / 3 + nu(x, beta) ** 2 / 36) * Omega(z, x, beta) ** (-1 / 3)
-        + Omega(z, x, beta) ** (1 / 3)
-    )
+    return (-nu(x, beta)/3
+        + (zeta(z, x, beta)/3 + nu(x, beta)**2/36) * Omega(z, x, beta)**(-1/3)
+        + Omega(z, x, beta)**(1/3))
 
-
-def alphaOld(z, x, beta):
-    """
-    Eq. (A4) from Ref[1]
-    Note that 'x' here corresponds to 'chi = x / rho' in the paper.
-    """
-    if z < 0:
-        out = (1/2* (-(2*m(z, x, beta))**(1/2)+ (-2*(m(z, x, beta) + nu(x, beta))
-                    + 2*eta(z, x, beta)*(2*m(z, x, beta))**(-1/2))**(1/2)))
-    else:
-        out = (
-            1
-            / 2
-            * (
-                (2 * m(z, x, beta)) ** (1 / 2)
-                + (
-                    -2 * (m(z, x, beta) + nu(x, beta))
-                    - 2 * eta(z, x, beta) * (2 * m(z, x, beta)) ** (-1 / 2)
-                )
-                ** (1 / 2)
-            )
-        )
-    return real(out)
 
 
 def alpha(z, x, beta):
     """
     Eq. (A4) from Ref[1]
-    Note that 'x' here corresponds to 'chi = x / rho' in the paper.
+    Note that 'x' here corresponds to 'chi = x/rho', 
+    and 'z' here corresponds to 'xi = z/2/rho' in the paper. 
     """
     arg1 = np.sqrt(2 * abs(m(z, x, beta)))
     arg2 = -2 * (m(z, x, beta) + nu(x, beta))
@@ -164,20 +123,18 @@ def alpha(z, x, beta):
     # out2 = real(1/2*( arg1 + np.sqrt( abs( arg2 - arg3) )))
     # return np.nan_to_num(np.where(z<0, out1, out2))
     return np.nan_to_num(
-        np.where(
-            z < 0,
+        np.where(z < 0,
             real(1 / 2 * (-arg1 + np.sqrt(abs(arg2 + arg3)))),
-            real(1 / 2 * (arg1 + np.sqrt(abs(arg2 - arg3)))),
-        )
-    )
+            real(1 / 2 * (arg1 + np.sqrt(abs(arg2 - arg3)))) ) )
 
 
 def kappa(z, x, beta):
     """
     Eq. (13) from Ref[1] with argumaent zeta = 0.
-    Note that 'x' here corresponds to 'chi = x / rho' in the paper.
+    Note that 'x' here corresponds to 'chi = x/rho', 
+    and 'z' here corresponds to 'xi = z/2/rho' in the paper. 
     """
-    return (x ** 2 + 4 * (1 + x) * sin(alpha(z, x, beta)) ** 2) ** (1 / 2)
+    return (x**2 + 4*(1+x) * sin(alpha(z, x, beta))**2)**(1/2)
 
 
 ### Functions below are obsolete
@@ -190,12 +147,10 @@ def lambda_p_Gauss(z, x):
     sigmaz = 10e-6
     sigmax = 10e-6
     return (
-        1
-        / (2 * pi * sigmaz * sigmax)
-        * exp(-x ** 2 / 2 / sigmax ** 2)
-        * exp(-z ** 2 / 2 / sigmaz ** 2)
-        * (-z / sigmaz ** 2)
-    )
+        1/(2*pi*sigmaz*sigmax)
+        * exp(-x**2 / 2 / sigmax**2)
+        * exp(-z**2 / 2 / sigmaz**2)
+        * (-z / sigmaz**2))
 
 
 def make_2dgrid(func, zmin, zmax, dz, xmin, xmax, dx):
--- csr2d/core_mpmath.py
+++ csr2d/core_mpmath.py
@@ -7,40 +7,27 @@ def psi_s(z, x, beta):
     2D longitudinal potential
     Eq. (23) from Ref[1] with no constant factor (e*beta**2/2/rho**2).
     Ref[1]: Y. Cai and Yuantao. Ding, PRAB 23, 014402 (2020)
-    Note that 'x' here corresponds to 'chi = x / rho' in the paper.
+    Note that 'x' here corresponds to 'chi = x/rho', 
+    and 'z' here corresponds to 'xi = z/2/rho' in the paper. 
     """
     out = (cos(2 * alpha(z, x, beta)) - 1 / (1 + x)) / (
-        kappa(z, x, beta) - beta * (1 + x) * sin(2 * alpha(z, x, beta))
-    )
+        kappa(z, x, beta) - beta * (1 + x) * sin(2 * alpha(z, x, beta)))
     return out
 
 
 def psi_x(z, x, beta):
     """
     Eq.(24) from Ref[1] with argument zeta=0 and no constant factor e*beta**2/2/rho**2.
-    Note that 'x' here corresponds to 'chi = x / rho' in the paper.
+    Note that 'x' here corresponds to 'chi = x/rho', 
+    and 'z' here corresponds to 'xi = z/2/rho' in the paper. 
     """
     kap = kappa(z, x, beta)
     alp = alpha(z, x, beta)
     arg2 = -4 * (1 + x) / x ** 2
 
-    T1 = (
-        1
-        / fabs(x)
-        / (1 + x)
-        * ((2 + 2 * x + x ** 2) * ellipf(alp, arg2) - x ** 2 * ellipe(alp, arg2))
-    )
-    D = kap ** 2 - beta ** 2 * (1 + x) ** 2 * sin(2 * alp) ** 2
-    T2 = (
-        (
-            kap ** 2
-            - 2 * beta ** 2 * (1 + x) ** 2
-            + beta ** 2 * (1 + x) * (2 + 2 * x + x ** 2) * cos(2 * alp)
-        )
-        / beta
-        / (1 + x)
-        / D
-    )
+    T1 = (1/fabs(x)/(1+x) * ((2 + 2*x + x**2) * ellipf(alp, arg2) - x**2 * ellipe(alp, arg2)))
+    D = kap**2 - beta**2 * (1 + x) ** 2 * sin(2 * alp)**2
+    T2 = ((kap**2- 2 * beta**2 * (1+x)**2 + beta**2 * (1+x) * (2 + 2*x + x**2) * cos(2 * alp))/ beta/ (1+x)/ D)
     T3 = -kap * sin(2 * alp) / D
     T4 = kap * beta ** 2 * (1 + x) * sin(2 * alp) * cos(2 * alp) / D
     T5 = 1 / fabs(x) * ellipf(alp, arg2)  # psi_phi without e/rho**2 factor
@@ -52,24 +39,27 @@ def psi_x(z, x, beta):
 def nu(x, beta):
     """
     Eq. (6) from Ref[1] (coeffient of alpha**2)
-    Note that 'x' here corresponds to 'chi = x / rho' in the paper.
+    Note that 'x' here corresponds to 'chi = x/rho' in the paper. 
     """
-    return 3 * (1 - beta ** 2 - beta ** 2 * x) / beta ** 2 / (1 + x)
+    return 3 * (1 - beta**2 - beta**2 * x) / beta**2 / (1+x)
 
 
 def eta(z, x, beta):
     """
     Eq. (6) from Ref[1] (coeffient of alpha)
+    Note that 'x' here corresponds to 'chi = x/rho', 
+    and 'z' here corresponds to 'xi = z/2/rho' in the paper. 
     """
-    return -6 * z / beta ** 2 / (1 + x)
+    return -6*z / beta**2 / (1+x)
 
 
 def zeta(z, x, beta):
     """
     Eq. (6) from Ref[1] (constant term)
-    Note that 'x' here corresponds to 'chi = x / rho' in the paper.
+    Note that 'x' here corresponds to 'chi = x/rho', 
+    and 'z' here corresponds to 'xi = z/2/rho' in the paper. 
     """
-    return 3 * (4 * z ** 2 - beta ** 2 * x ** 2) / 4 / beta ** 2 / (1 + x)
+    return 3*(4 * z**2 - beta**2 * x**2) / 4 / beta**2 / (1+x)
 
 
 def Omega(z, x, beta):
@@ -77,32 +67,30 @@ def Omega(z, x, beta):
     Eq. (A3) from Ref[1]
     Note that 'x' here corresponds to 'chi = x / rho' in the paper.
     """
-    temp = (
-        eta(z, x, beta) ** 2 / 16
+    temp = (eta(z, x, beta)**2 / 16
         - zeta(z, x, beta) * nu(x, beta) / 6
-        + nu(x, beta) ** 3 / 216
-    )
-    return temp + (temp ** 2 - (zeta(z, x, beta) / 3 + nu(x, beta) ** 2 / 36) ** 3) ** (
-        1 / 2
-    )
+        + nu(x, beta)**3 / 216)
+    return temp + (temp**2 - (zeta(z, x, beta)/ 3 + nu(x, beta)**2 / 36)**3)**(1/2)
 
 
 def m(z, x, beta):
     """
     Eq. (A2) from Ref[1]
-    Note that 'x' here corresponds to 'chi = x / rho' in the paper.
+    Note that 'x' here corresponds to 'chi = x/rho', 
+    and 'z' here corresponds to 'xi = z/2/rho' in the paper. 
     """
     return (
         -nu(x, beta) / 3
-        + (zeta(z, x, beta) / 3 + nu(x, beta) ** 2 / 36) * Omega(z, x, beta) ** (-1 / 3)
-        + Omega(z, x, beta) ** (1 / 3)
+        + (zeta(z, x, beta) / 3 + nu(x, beta)**2 / 36) * Omega(z, x, beta)**(-1 / 3)
+        + Omega(z, x, beta)**(1 / 3)
     )
 
 
 def alpha(z, x, beta):
     """
     Eq. (A4) from Ref[1]
-    Note that 'x' here corresponds to 'chi = x / rho' in the paper.
+    Note that 'x' here corresponds to 'chi = x/rho', 
+    and 'z' here corresponds to 'xi = z/2/rho' in the paper. 
     """
     arg1 = sqrt(2 * fabs(m(z, x, beta)))
     arg2 = -2 * (m(z, x, beta) + nu(x, beta))
@@ -117,31 +105,26 @@ def alpha(z, x, beta):
 def kappa(z, x, beta):
     """
     Eq. (13) from Ref[1] with argumaent zeta = 0
-    Note that 'x' here corresponds to 'chi = x / rho' in the paper.
+    Note that 'x' here corresponds to 'chi = x/rho', 
+    and 'z' here corresponds to 'xi = z/2/rho' in the paper. 
     """
-    return (x ** 2 + 4 * (1 + x) * sin(alpha(z, x, beta)) ** 2) ** (1 / 2)
+    return (x**2 + 4*(1+x) * sin(alpha(z, x, beta))**2)**(1/2)
 
 
 def lambda_p_gauss(z, x, sigmaz, sigmax):
     """
     The z derivative of a 2D Gaussian G(z,x)
     """
-    return (
-        1
-        / (2 * pi * sigmaz * sigmax)
-        * exp(-x ** 2 / 2 / sigmax ** 2)
-        * exp(-z ** 2 / 2 / sigmaz ** 2)
-        * (-z / sigmaz ** 2)
-    )
+    return (1 / (2 * pi * sigmaz * sigmax)
+        * exp(-x**2 / 2 / sigmax**2)
+        * exp(-z**2 / 2 / sigmaz**2)
+        * (-z / sigmaz ** 2))
 
 
 def lambda_gauss(z, x, sigmaz, sigmax):
     """
     The 2D Gaussian G(z,x)
     """
-    return (
-        1
-        / (2 * pi * sigmaz * sigmax)
-        * exp(-x ** 2 / 2 / sigmax ** 2)
-        * exp(-z ** 2 / 2 / sigmaz ** 2)
-    )
+    return (1 / (2 * pi * sigmaz * sigmax)
+        * exp(-x**2 / 2 / sigmax**2)
+        * exp(-z**2 / 2 / sigmaz**2))
